5e472125b3940c551d67a93c361eef28bf0644d7
inknsharps <nchen25@gmail.com>
2021-03-02 23:51:25 -0500

    add base search history functionality

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,39 @@
 // Query selectors for existing elements on the page
 let searchFieldEl = document.querySelector(".search-field");
 let searchButtonEl = document.querySelector(".submit-search");
+let searchHistoryEl = document.querySelector(".search-history");
 
-// Function to call the current weather data, with the argument for the city
-function callWeather(cityString){
-    let currentWeatherData = `https://api.openweathermap.org/data/2.5/weather?q=${cityString}&appid=704f3b5ee25c5694ae0db66afd13ab60`
-    console.log(currentWeatherData);
-    fetch(currentWeatherData)
+// Function to build li elements for the search history
+function buildSearchHistory(city){
+    const li = document.createElement("li");
+    li.className = "list-group-item";
+    const button = document.createElement("button");
+    button.className = "btn btn-outline-secondary submit-saved-city";
+    button.setAttribute("type", "button");
+    button.textContent = city;
+    li.appendChild(button);
+    searchHistoryEl.appendChild(li);
+}
+
+// Function to call the current weather data (currently in metric), with the argument for the city
+function callWeather(city){
+    let calledWeatherData = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=704f3b5ee25c5694ae0db66afd13ab60&units=metric`
+    console.log(calledWeatherData);
+    fetch(calledWeatherData)
         .then((response) => { 
             return response.json();
         })
-        .then((data) => {
-            console.log(data);
+        .then((weatherData) => {
+            console.log(weatherData);
+
         });
 }
 
 // Function to search for a city
 function searchCity(){
     let searchValue = searchFieldEl.value;
+    callWeather(searchValue);
+    buildSearchHistory(searchValue);
 }
 
 // Event Listener for the search button
